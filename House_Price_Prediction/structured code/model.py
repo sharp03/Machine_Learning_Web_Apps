258d69e00388ecbb2b84bb22561b1e00f5775396
Prashant Sharma <prashantsharma1457@gmail.com>
2020-05-09 15:22:48 +0530

    New updates

---
+++ House_Price_Prediction/structured code/model.py
@@ -0,0 +1,7 @@
+# Import the modules
+# Data handle
+import pandas as pd
+import numpy as np
+# Data visualization
+import matplotlib.pyplot as plt
+# Machine Learning
---
+++ House_Price_Prediction/structured code/parameters_class.py
@@ -0,0 +1,8 @@
+class arguments():
+    def __init__(self):
+        self.data_file = r"F:\Data Science Related\Machine_Learning_Web_Apps\House_Price_Prediction\data\kc_house_data.csv.xlsx"
+        self.drop_col = ['']
+        self.preview = True
+        # Machine learning
+        self.figsize = (15, 5)
+        self.plot_data = True
